40334a6457f36dc4b2796b94f7df0550a15eb366
Ruben Arteaga <rubentec3@gmail.com>
2024-01-03 19:44:46 -0400

    inicio del proyecto

---
+++ SIS_457_Ferreteria_verano/CNegocios/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNegocios
+{
+    public class Class1
+    {
+    }
+}
