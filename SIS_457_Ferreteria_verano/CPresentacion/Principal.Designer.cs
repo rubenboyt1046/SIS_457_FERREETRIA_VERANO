b3ebaa48b7fa72234fcb80727d0e0b0005d7b3a0
Ruben Arteaga <rubentec3@gmail.com>
2024-01-08 18:32:35 -0400

    mejoras de iconos

--- SIS_457_Ferreteria_verano/CPresentacion/Principal.Designer.cs
+++ SIS_457_Ferreteria_verano/CPresentacion/Principal.Designer.cs
@@ -32,6 +32,8 @@
             this.btnEmpleados = new System.Windows.Forms.Button();
             this.btnClientes = new System.Windows.Forms.Button();
             this.btnProdcutos = new System.Windows.Forms.Button();
+            this.btnVentas = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnEmpleados
@@ -47,9 +49,10 @@
             this.btnEmpleados.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnEmpleados.Location = new System.Drawing.Point(23, 155);
             this.btnEmpleados.Name = "btnEmpleados";
-            this.btnEmpleados.Size = new System.Drawing.Size(214, 30);
+            this.btnEmpleados.Size = new System.Drawing.Size(138, 30);
             this.btnEmpleados.TabIndex = 6;
             this.btnEmpleados.Text = "Empleados";
+            this.btnEmpleados.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnEmpleados.UseVisualStyleBackColor = false;
             this.btnEmpleados.Click += new System.EventHandler(this.btnEmpleados_Click);
             // 
@@ -66,9 +69,10 @@
             this.btnClientes.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnClientes.Location = new System.Drawing.Point(23, 119);
             this.btnClientes.Name = "btnClientes";
-            this.btnClientes.Size = new System.Drawing.Size(214, 30);
+            this.btnClientes.Size = new System.Drawing.Size(138, 30);
             this.btnClientes.TabIndex = 7;
             this.btnClientes.Text = "Clientes";
+            this.btnClientes.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnClientes.UseVisualStyleBackColor = false;
             // 
             // btnProdcutos
@@ -82,12 +86,51 @@
             this.btnProdcutos.ForeColor = System.Drawing.Color.White;
             this.btnProdcutos.Image = ((System.Drawing.Image)(resources.GetObject("btnProdcutos.Image")));
             this.btnProdcutos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnProdcutos.Location = new System.Drawing.Point(23, 83);
+            this.btnProdcutos.Location = new System.Drawing.Point(23, 72);
             this.btnProdcutos.Name = "btnProdcutos";
-            this.btnProdcutos.Size = new System.Drawing.Size(214, 30);
+            this.btnProdcutos.Size = new System.Drawing.Size(138, 41);
             this.btnProdcutos.TabIndex = 5;
             this.btnProdcutos.Text = "Productos";
+            this.btnProdcutos.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnProdcutos.UseVisualStyleBackColor = false;
+            // 
+            // btnVentas
+            // 
+            this.btnVentas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnVentas.FlatAppearance.BorderSize = 0;
+            this.btnVentas.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnVentas.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.btnVentas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVentas.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVentas.ForeColor = System.Drawing.Color.White;
+            this.btnVentas.Image = ((System.Drawing.Image)(resources.GetObject("btnVentas.Image")));
+            this.btnVentas.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnVentas.Location = new System.Drawing.Point(23, 191);
+            this.btnVentas.Name = "btnVentas";
+            this.btnVentas.Size = new System.Drawing.Size(138, 30);
+            this.btnVentas.TabIndex = 8;
+            this.btnVentas.Text = "Ventas";
+            this.btnVentas.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnVentas.UseVisualStyleBackColor = false;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnCerrar.FlatAppearance.BorderSize = 0;
+            this.btnCerrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnCerrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(80)))), ((int)(((byte)(200)))));
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCerrar.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.ForeColor = System.Drawing.Color.White;
+            this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
+            this.btnCerrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCerrar.Location = new System.Drawing.Point(23, 227);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(138, 30);
+            this.btnCerrar.TabIndex = 9;
+            this.btnCerrar.Text = "Cerrar sesion";
+            this.btnCerrar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCerrar.UseVisualStyleBackColor = false;
             // 
             // Principal
             // 
@@ -95,6 +138,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnVentas);
             this.Controls.Add(this.btnEmpleados);
             this.Controls.Add(this.btnClientes);
             this.Controls.Add(this.btnProdcutos);
@@ -109,5 +154,7 @@
         private System.Windows.Forms.Button btnEmpleados;
         private System.Windows.Forms.Button btnClientes;
         private System.Windows.Forms.Button btnProdcutos;
+        private System.Windows.Forms.Button btnVentas;
+        private System.Windows.Forms.Button btnCerrar;
     }
 }
